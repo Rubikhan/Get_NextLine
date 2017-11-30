2e021dd6aaac41d2d425005e263ac183d9ca10bb
Rubikhan <sammaddux@gmail.com>
2017-11-29 16:02:49 -0800

    fails different tests depending on BUFF_SIZE macro value

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: smaddux <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/27 13:53:35 by smaddux           #+#    #+#             */
-/*   Updated: 2017/11/28 18:57:50 by smaddux          ###   ########.fr       */
+/*   Updated: 2017/11/29 15:54:28 by smaddux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,11 +49,16 @@ static int first_entry(int fd, char **line, char **fda, int rv)
 	if((rv = read(fd, str, BUFF_SIZE)) == -1) 
 		return (-1);
 //	printf("FIRSTENTRY: %s", str);
-	if(fda[0])
+	if (fda[0] && ft_strchr(fda[0], '\n'))
+	{
+		line[0] = olas(fda[0]);
+		return(1);
+	}
+	else if(fda[0])
 		fda[0] = ft_strjoin(fda[0], str);
 	else
 		fda[0] = ft_strdup(str);;
-	if (rv == 0)
+if (rv == 0)
 	{
 		line[0] = olas(fda[0]);
 		return (0);
@@ -89,6 +94,19 @@ int get_next_line(const int fd, char **line)
 	return(rv);
 
 } 
+
+/* int main(int argc, char **argv) */
+/* { */
+/*     char *str; */
+/*     int fd; */
+
+/*     fd = open(argv[1], O_RDONLY); */
+/*     while (get_next_line(fd, &str)) */
+/*     { */
+/*         printf("%s\n", str); */
+/*         str = NULL; */
+/*     } */
+/* } */
 
 /*       int main(int argc, char *argv[])       */
 /*       {       */
